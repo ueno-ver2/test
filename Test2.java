899e6346f0fb0d050e795b2a10c93e26369de266
unknown <>
2016-07-11 12:57:22 +0900

    Javaのテスト

---
+++ Test2.java
@@ -0,0 +1,9 @@
+public class Test2{
+public static void main (String[] args){
+
+for(int i = 0; i < 10; ){
+System.out.println(++i);
+}
+}
+}
+
---
+++ src/Test3.java
@@ -0,0 +1,11 @@
+public class Test3{
+
+public static void main(String[] args){
+
+String name = "taro";
+
+System.out.println("my name is " + name );
+
+}
+
+}
